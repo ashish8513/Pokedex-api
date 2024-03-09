8199be3f454374ffc603f0dad21414246a2995a2
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-03-09 17:22:59 +0530

    added some changes on details

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 import Pokedex from './Components/Pokedex/Pokedex'
+import CustomRoutes from './Routes/CustomRoutes'
 
 
 function App() {
 
 
   return (
-    <>
-   <Pokedex />
+    <div className='outer-pokedex'>
+      <h1 id='pokedex-heading'>
+        <Link to='/'>
+          Pokedex
+        </Link>
+      </h1>
+      <CustomRoutes />
 
-    </>
+    </div>
   )
 }
 
---
+++ src/Components/PokemonDetails/PokemonDetails.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import './PokemonDetail.css'
+
+function PokemonDetails() {
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState({})
+  console.log(id)
+
+  async function downloadPokemon() {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    setPokemon({
+      name: response.data.name,
+      image: response.data.sprites.other.dream_world.front_default,
+      weight: response.data.weight,
+      height: response.data.height,
+      types: response.data.types.map((t) => t.type.name)
+      // stats:response.data.stats
+    })
+
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    downloadPokemon();
+  }, [])
+  return (
+    <div className="pokemon-details-wrapper">
+      <img className="pokemon-image" src={pokemon.image}  />
+      <div className="pokemon-details-name"><span>{pokemon.name}</span></div>
+      <div className="pokemon-details-name">Height: {pokemon.height}</div>
+      <div className="pokemon-details-name">Weight: {pokemon.weight} </div>
+      <div className="pokemon-types">
+        {pokemon.types && pokemon.types.map((t)=><div key={t}>{t}</div>)}
+    </div>
+    </div>
+  )
+}
+
+export default PokemonDetails
